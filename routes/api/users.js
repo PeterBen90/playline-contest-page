5b61c3fa287201802e811b9d9f5b82d1dcfb862d
Peter Benvenuto <peterbenvenuto90@yahoo.ca>
2018-06-28 21:45:17 -0400

    added post request and email validation

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,44 @@
 const express = require('express');
 const router = express.Router();
 
+//Load input validation
+const validateRegisterInput = require('../../validation/register');
+
+//Load user model
+const User = require('../../models/User');
+
 //@route   GET request to api/users/test
 //@desc    Tests users route
 //@access  Public route
 router.get('/test', (req, res) => res.json({ msg: 'Users works' }));
+
+//@route   POST request to api/users/register
+//@desc    Register user
+//@access  Public route
+router.post('/register', (req, res) => {
+  const { errors, isValid } = validateRegisterInput(req.body);
+
+  //Check validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+  User.findOne({ email: req.body.email }).then(user => {
+    if (user) {
+      errors.email = 'Email already exists';
+      return res.status(400).json(errors);
+    } else {
+      const newUser = new User({
+        firstname: req.body.firstname,
+        lastname: req.body.lastname,
+        email: req.body.email
+      });
+
+      newUser
+        .save()
+        .then(user => res.json(user))
+        .catch(err => console.log(err));
+    }
+  });
+});
 
 module.exports = router;
---
+++ validation/register.js
@@ -0,0 +1,23 @@
+const Validator = require('validator');
+const isEmpty = require('./is-empty');
+
+module.exports = validateRegisterInput = data => {
+  let errors = {};
+
+  //If not empty it will be whatever data.email is
+  //If it is empty then it will be an empty string and get tested below
+  data.email = !isEmpty(data.email) ? data.email : '';
+
+  if (Validator.isEmpty(data.email)) {
+    errors.email = 'Email field is required';
+  }
+
+  if (!Validator.isEmail(data.email)) {
+    errors.email = 'Email is invalid';
+  }
+
+  return {
+    errors,
+    isValid: isEmpty(errors)
+  };
+};
