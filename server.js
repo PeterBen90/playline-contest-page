a9a993983d6f3fdda265faa1c81d4a506dcba85c
Peter Benvenuto <peterbenvenuto90@yahoo.ca>
2018-07-02 20:14:18 -0400

    cahnegd background

--- server.js
+++ server.js
@@ -13,6 +13,17 @@ app.use(bodyParser.json());
 //connect to files in public folder
 app.use(express.static('public'));
 
+// CORS
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+  if (req.method === 'OPTIONS') {
+    return res.send(204);
+  }
+  next();
+});
+
 //DB config
 const db = require('./config/keys').mongoURI;
 
